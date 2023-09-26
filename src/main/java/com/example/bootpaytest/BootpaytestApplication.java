03a92313b4e8f4bb242441a8fcbc0c9a58d49e4c
tero1115 <limjisang1115@gmail.com>
2023-09-26 09:23:02 +0900

    init

---
+++ src/main/java/com/example/bootpaytest/BootpaytestApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootpaytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootpaytestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootpaytestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bootpaytest/BootpaytestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootpaytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootpaytestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
